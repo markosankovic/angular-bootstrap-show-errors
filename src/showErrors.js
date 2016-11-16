6c2ea74b3ec6a20f95c4a62e419f0dad00d45dab
Marko Sanković <sankovic.marko@gmail.com>
2016-11-16 12:38:18 +0100

    Fix: show-errors element has no child input elements with a 'name' attribute and a 'form-control'

--- src/showErrors.js
+++ src/showErrors.js
@@ -23,45 +23,47 @@
         return showSuccess;
       };
       linkFn = function(scope, el, attrs, formCtrl) {
-        var blurred, inputEl, inputName, inputNgEl, options, showSuccess, toggleClasses, trigger;
-        blurred = false;
-        options = scope.$eval(attrs.showErrors);
-        showSuccess = getShowSuccess(options);
-        trigger = getTrigger(options);
-        inputEl = el[0].querySelector('.form-control[name]');
-        inputNgEl = angular.element(inputEl);
-        inputName = $interpolate(inputNgEl.attr('name') || '')(scope);
-        if (!inputName) {
-          throw "show-errors element has no child input elements with a 'name' attribute and a 'form-control' class";
-        }
-        inputNgEl.bind(trigger, function() {
-          blurred = true;
-          return toggleClasses(formCtrl[inputName].$invalid);
-        });
-        scope.$watch(function() {
-          return formCtrl[inputName] && formCtrl[inputName].$invalid;
-        }, function(invalid) {
-          if (!blurred) {
-            return;
+        $timeout(function() {
+          var blurred, inputEl, inputName, inputNgEl, options, showSuccess, toggleClasses, trigger;
+          blurred = false;
+          options = scope.$eval(attrs.showErrors);
+          showSuccess = getShowSuccess(options);
+          trigger = getTrigger(options);
+          inputEl = el[0].querySelector('.form-control[name]');
+          inputNgEl = angular.element(inputEl);
+          inputName = $interpolate(inputNgEl.attr('name') || '')(scope);
+          if (!inputName) {
+            throw "show-errors element has no child input elements with a 'name' attribute and a 'form-control' class";
           }
-          return toggleClasses(invalid);
+          inputNgEl.bind(trigger, function() {
+            blurred = true;
+            return toggleClasses(formCtrl[inputName].$invalid);
+          });
+          scope.$watch(function() {
+            return formCtrl[inputName] && formCtrl[inputName].$invalid;
+          }, function(invalid) {
+            if (!blurred) {
+              return;
+            }
+            return toggleClasses(invalid);
+          });
+          scope.$on('show-errors-check-validity', function() {
+            return toggleClasses(formCtrl[inputName].$invalid);
+          });
+          scope.$on('show-errors-reset', function() {
+            return $timeout(function() {
+              el.removeClass('has-error');
+              el.removeClass('has-success');
+              return blurred = false;
+            }, 0, false);
+          });
+          return toggleClasses = function(invalid) {
+            el.toggleClass('has-error', invalid);
+            if (showSuccess) {
+              return el.toggleClass('has-success', !invalid);
+            }
+          };
         });
-        scope.$on('show-errors-check-validity', function() {
-          return toggleClasses(formCtrl[inputName].$invalid);
-        });
-        scope.$on('show-errors-reset', function() {
-          return $timeout(function() {
-            el.removeClass('has-error');
-            el.removeClass('has-success');
-            return blurred = false;
-          }, 0, false);
-        });
-        return toggleClasses = function(invalid) {
-          el.toggleClass('has-error', invalid);
-          if (showSuccess) {
-            return el.toggleClass('has-success', !invalid);
-          }
-        };
       };
       return {
         restrict: 'A',
